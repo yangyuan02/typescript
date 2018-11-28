76fc8398b90d64d14e9bec29c21374efefcee93e
yangyuan <1367511704@qq.com>
2018-11-28 11:13:55 +0800

    feat(外部枚举)

--- 07.ts
+++ 07.ts
@@ -14,3 +14,22 @@ console.log(Days["Sat"] === 6)//true
 //典型的计算所得项
 // enum Color {Red, Green, Blue = "blue".length};//"blue".length是计算所得项
 // enum Color {Red = "red".length, Green, Blue}; //error后面没有手动赋值的初始值报错
+
+//常数枚举
+// const enum Directions {
+//     Up,
+//     Down,
+//     Left,
+//     Right
+// };
+// let directions = [Directions.Up, Directions.Down, Directions.Left, Directions.Right];
+// const enum Color {Red, Green, Blue = "blue".length}; //error
+
+// 外部枚举 declare enum定义的
+declare enum Directions {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
